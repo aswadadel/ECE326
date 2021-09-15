bddf8756b4eb11bcc9aa5bfe399081f1057a5827
Adel Aswad <aswadadel@gmail.com>
2021-09-15 17:47:27 -0400

    Initial code for Assignment 1

---
+++ asst1/easydb/easydb.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+#
+# easydb.py
+#
+# Definition for the Database class in EasyDB client
+#
+
+import socket
+from .packet import EXIT, OK, SERVER_BUSY, request, response
+from .exception import PacketError
+
+class Database:
+    def __repr__(self):
+        return "<EasyDB Database object>"
+
+    def __init__(self, tables):    
+        self._socket = None
+        # TODO: implement me
+        
+    def connect(self, host, port):
+        assert(self._socket is None)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, int(port)))
+        
+        code = response(self._socket)
+        if code == OK:
+            return True
+        elif code == SERVER_BUSY:
+            self._socket.close()
+            self._socket = None
+            return False
+        else:
+            raise PacketError("Unexpected code %d during connect()"%code)
+
+    def close(self):
+        if self._socket is None:
+            return
+        request(EXIT, 0)
+        self._socket.close()
+        self._socket = None
+
+    def __str__(self):
+        # TODO: implement me
+        return ""
+
+    def insert(self, table_name, values):
+        # TODO: implement me
+        pass
+
+    def update(self, table_name, pk, values, version=None):
+        # TODO: implement me
+        pass
+
+    def drop(self, table_name, pk):
+        # TODO: implement me
+        pass
+        
+    def get(self, table_name, pk):
+        # TODO: implement me
+        pass
+
+    def scan(self, table_name, op, column_name=None, value=None):
+        # TODO: implement me
+        pass
+                        
---
+++ asst1/easydb/packet.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+#
+# packet.py
+#
+# Definition for all the packet-related constants and classes in EasyDB
+#
+
+import struct
+
+# request commands
+INSERT = 1
+UPDATE = 2
+DROP = 3
+GET = 4    
+SCAN = 5
+EXIT = 6
+
+# response codes
+OK = 1
+NOT_FOUND = 2
+BAD_TABLE = 3
+BAD_QUERY = 4 
+TXN_ABORT = 5
+BAD_VALUE = 6
+BAD_ROW = 7
+BAD_REQUEST = 8
+BAD_FOREIGN = 9
+SERVER_BUSY = 10
+UNIMPLEMENTED = 11
+
+# column types
+NULL = 0
+INTEGER = 1
+FLOAT = 2
+STRING = 3
+FOREIGN = 4
+
+# operator types
+class operator:
+    AL = 1  # everything
+    EQ = 2  # equal 
+    NE = 3  # not equal
+    LT = 4  # less than
+    GT = 5  # greater than 
+    LE = 6  # you do not have to implement the following two
+    GE = 7
+    
+# TODO: refactor so that it can send commands with arguments 
+def request(sock, command, table_nr=0):
+    # sending struct request to server
+    buf = struct.pack("!ii", command, table_nr)
+    sock.send(buf)
+    
+# TODO: refactor so that it can receive response with arguments
+def response(sock):
+    # expecting struct response, which is 4 bytes
+    buf = sock.recv(4)
+    return struct.unpack("!i", buf)[0]
+
