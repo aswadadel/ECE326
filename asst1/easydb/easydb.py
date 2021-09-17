17d333357079269362c239128ab7add31d2831f7
Adel Aswad <aswadadel@gmail.com>
2021-09-17 16:26:53 -0400

    pass db validation

--- asst1/easydb/easydb.py
+++ asst1/easydb/easydb.py
@@ -7,7 +7,8 @@
 
 import socket
 from .packet import EXIT, OK, SERVER_BUSY, request, response
-from .exception import PacketError
+from .exception import IntegrityError, PacketError
+import re
 
 class Database:
     def __repr__(self):
@@ -15,7 +16,47 @@ class Database:
 
     def __init__(self, tables):    
         self._socket = None
-        # TODO: implement me
+        self._tables = set()
+        try:
+            iterator = iter(tables)
+        except:
+            raise TypeError()
+        # try:
+        # used to check if foreign key columns refer to existing tables
+        tableNames = set()
+        # used to check for duplicate column names
+        columnNames = set()
+        for table in tables:
+            # check if table name is string and isn't duplicated
+            if not isinstance(table[0], str):
+                raise TypeError()
+            if not re.match(r"[a-zA-Z][a-zA-Z0-9_]*", table[0]) or table[0] in tableNames:
+                raise ValueError()
+            tableNames.add(table[0])
+            columnNames.clear()
+            # print(table[0])
+            for column in table[1]:
+                # check if column name is string
+                if not isinstance(column[0], str):
+                    raise TypeError()
+                if not re.match(r"[a-zA-Z][a-zA-Z0-9_]*", column[0]) or column[0] == 'id':
+                    raise ValueError()
+                # check if column value is set to str, int, float, or a string
+                if column[0] in columnNames \
+                    or (column[1] not in (str,float,int) and not isinstance(column[1], str)):  
+                    raise ValueError()
+                if isinstance(column[1], str) and (column[1] not in tableNames or column[1] == table[0]):
+                    raise IntegrityError()
+                columnNames.add(column[0])
+        # except TypeError:
+        #     print("TypeError")
+        # except ValueError:
+        #     print("ValueError")
+        # except IntegrityError:
+        #     print("IntegrityError")
+        # except:
+        #     print("IndexError")
+                
         
     def connect(self, host, port):
         assert(self._socket is None)
