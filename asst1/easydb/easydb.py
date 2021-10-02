abaaa8b81738c8d811aaa9534e8e93a972011b82
Max Arreaga <max.arreaga@mail.utoronto.ca>
2021-10-01 21:56:08 -0400

    semi working update, struggling with foreign keys, committing for a checkpoint

--- asst1/easydb/easydb.py
+++ asst1/easydb/easydb.py
@@ -8,15 +8,16 @@
 import socket
 from struct import pack
 from typing import Dict
-from .packet import DROP, EXIT, INSERT, OK, SERVER_BUSY, request, response
-from .exception import IntegrityError, PacketError
+from .packet import DROP, EXIT, INSERT, OK, SERVER_BUSY, request, response, UPDATE, GET
+from .exception import IntegrityError, InvalidReference, PacketError
 import re
+
 
 class Database:
     def __repr__(self):
         return "<EasyDB Database object>"
 
-    def __init__(self, tables):    
+    def __init__(self, tables):
         self._socket = None
         # self._tables = set()
         try:
@@ -44,7 +45,7 @@ class Database:
                     raise ValueError()
                 # check if column value is set to str, int, float, or a foreign key, and is not a duplicate
                 if column[0] in columnNames \
-                    or (column[1] not in (str,float,int) and not isinstance(column[1], str)):  
+                        or (column[1] not in (str, float, int) and not isinstance(column[1], str)):
                     raise ValueError()
                 # check if the foreign key exists and is not self-referencing
                 if isinstance(column[1], str) and (column[1] not in tableNames or column[1] == table[0]):
@@ -52,12 +53,12 @@ class Database:
                 columnNames.add(column[0])
         self._tables = tables
         self._tableNames = tableNames
-        
+
     def connect(self, host, port):
         assert(self._socket is None)
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((host, int(port)))
-        
+
         code = response(self._socket)
         if code == OK:
             return True
@@ -66,7 +67,7 @@ class Database:
             self._socket = None
             return False
         else:
-            raise PacketError("Unexpected code %d during connect()"%code)
+            raise PacketError("Unexpected code %d during connect()" % code)
 
     def close(self):
         if self._socket is None:
@@ -74,7 +75,7 @@ class Database:
         request(self._socket, EXIT, 0)
         self._socket.close()
         self._socket = None
-    
+
     def __getTableIndex(self, tableName):
         tables = self._tables
         tableIndex = None
@@ -95,13 +96,13 @@ class Database:
         # the string returned by function
         tablesString = ''
         for table in tables:
-            tablesString+="%s {\n"%(table[0])
+            tablesString += "%s {\n" % (table[0])
             for column in table[1]:
                 columnType = column[1]
                 if column[1] in typesDict:
                     columnType = typesDict[column[1]]
-                tablesString += "%s: %s;\n"%(column[0],  columnType)
-            tablesString+='}\n'
+                tablesString += "%s: %s;\n" % (column[0],  columnType)
+            tablesString += '}\n'
         return tablesString
 
     def insert(self, table_name, values):
@@ -111,14 +112,28 @@ class Database:
             raise PacketError()
         for index, column in enumerate(tables[tableIndex][1]):
             if (isinstance(column[1], str) and not isinstance(values[index], int)) \
-                or (not isinstance(column[1], str) and not isinstance(values[index], column[1])):
+                    or (not isinstance(column[1], str) and not isinstance(values[index], column[1])):
                 raise PacketError()
-        request(self._socket, INSERT, table_nr=tableIndex+1, columns=tables[tableIndex][1], values=values)
+        request(self._socket, INSERT, table_nr=tableIndex+1,
+                columns=tables[tableIndex][1], values=values)
         return response(self._socket, INSERT)
 
-    def update(self, table_name, pk, values, version=None):
-        # TODO: implement me
-        pass
+    def update(self, table_name, pk, values, version=0):
+        tables = self._tables
+        tableIndex = self.__getTableIndex(table_name)
+        if not isinstance(pk, int):
+            raise PacketError()
+        if not isinstance(version, int):
+            raise PacketError()
+        if len(values) != len(tables[tableIndex][1]):
+            raise PacketError()
+        for index, column in enumerate(tables[tableIndex][1]):
+            if (isinstance(column[1], str) and not isinstance(values[index], int)) \
+                    or (not isinstance(column[1], str) and not isinstance(values[index], column[1])):
+                raise PacketError()
+        request(self._socket, command=UPDATE, table_nr=tableIndex+1,
+                columns=tables[tableIndex][1], values=values, version=version, pk=pk)
+        return response(self._socket, command=UPDATE)
 
     def drop(self, table_name, pk):
         if not isinstance(pk, int):
@@ -126,12 +141,14 @@ class Database:
         tableIndex = self.__getTableIndex(table_name)
         request(self._socket, DROP, tableIndex+1, pk=pk)
         response(self._socket, DROP)
-        
+
     def get(self, table_name, pk):
-        # TODO: implement me
-        pass
+        if not isinstance(pk, int):
+            raise PacketError()
+        tableIndex = self.__getTableIndex(table_name)
+        request(self._socket, GET, tableIndex+1, pk=pk)
+        return response(self._socket, GET)
 
     def scan(self, table_name, op, column_name=None, value=None):
         # TODO: implement me
         pass
-                        
--- asst1/easydb/packet.py
+++ asst1/easydb/packet.py
@@ -12,7 +12,7 @@ import struct
 INSERT = 1
 UPDATE = 2
 DROP = 3
-GET = 4    
+GET = 4
 SCAN = 5
 EXIT = 6
 
@@ -20,7 +20,7 @@ EXIT = 6
 OK = 1
 NOT_FOUND = 2
 BAD_TABLE = 3
-BAD_QUERY = 4 
+BAD_QUERY = 4
 TXN_ABORT = 5
 BAD_VALUE = 6
 BAD_ROW = 7
@@ -37,14 +37,17 @@ STRING = 3
 FOREIGN = 4
 
 # operator types
+
+
 class operator:
     AL = 1  # everything
-    EQ = 2  # equal 
+    EQ = 2  # equal
     NE = 3  # not equal
     LT = 4  # less than
-    GT = 5  # greater than 
+    GT = 5  # greater than
     LE = 6  # you do not have to implement the following two
     GE = 7
+
 
 # db error codes -> python exceptions
 responseCodeErrors = {
@@ -52,15 +55,16 @@ responseCodeErrors = {
     BAD_FOREIGN: InvalidReference,
     TXN_ABORT: TransactionAbort,
 }
-responseCodeErrors.update(dict.fromkeys(\
+responseCodeErrors.update(dict.fromkeys(
     [BAD_QUERY, BAD_REQUEST, BAD_ROW, BAD_TABLE, BAD_VALUE], PacketError))
 
 # python types -> db types
 columnDict = {
-    int:INTEGER,
+    int: INTEGER,
     float: FLOAT,
-    str:STRING,
+    str: STRING,
 }
+
 
 def insertReq(tableNumber, columns=None, values=None):
     request = struct.pack("!ii", INSERT, tableNumber)
@@ -78,40 +82,89 @@ def insertReq(tableNumber, columns=None, values=None):
             elif typePack == FLOAT:
                 typeSymbol = 'd'
             elif typePack == STRING:
-                size =len(values[index]) 
-                padding =  (4-size%4)%4 
-                typeSymbol = "%ds"%(size) + ("%dx"%(padding) if padding != 0 else '')
-                sizePack = size + padding 
+                size = len(values[index])
+                padding = (4-size % 4) % 4
+                typeSymbol = "%ds" % (size) + ("%dx" %
+                                               (padding) if padding != 0 else '')
+                sizePack = size + padding
                 valuePack = values[index].encode()
         # use this to print the row's details before they get packed
         # print("!ii%s"%(typeSymbol), typePack, sizePack, valuePack)
-        rows.append(struct.pack("!ii%s"%(typeSymbol), typePack, sizePack, valuePack))
-    return b''.join([request, count, b''.join(rows)])
+        rows.append(struct.pack("!ii%s" %
+                    (typeSymbol), typePack, sizePack, valuePack))
+    result = b''.join([request, count, b''.join(rows)])
+    print(result)
+    return result
+
 
 def dropReq(tableNumber, pk=None):
     request = struct.pack("!ii", DROP, tableNumber)
     index = struct.pack("!Q", pk)
     return b''.join([request, index])
 
+
 def exitReq(tableNumber):
     return struct.pack('!ii', EXIT, tableNumber)
+
+
+def updateReq(tableNumber, columns=None, values=None, version=0, pk=None):
+    request = struct.pack("!ii", UPDATE, tableNumber)
+    count = struct.pack("!i", len(columns))
+    key = struct.pack("!QQ", pk, version)
+    print("packing")
+    rows = list()
+    for index, column in enumerate(columns):
+        typePack = FOREIGN
+        sizePack = 8
+        valuePack = values[index]
+        typeSymbol = 'Q'
+        if column[1] in columnDict:
+            typePack = columnDict[column[1]]
+            if typePack == INTEGER:
+                typeSymbol = 'q'
+            elif typePack == FLOAT:
+                typeSymbol = 'd'
+            elif typePack == STRING:
+                size = len(values[index])
+                padding = (4-size % 4) % 4
+                typeSymbol = "%ds" % (size) + ("%dx" %
+                                               (padding) if padding != 0 else '')
+                sizePack = size + padding
+                valuePack = values[index].encode()
+        # use this to print the row's details before they get packed
+        # print("!ii%s"%(typeSymbol), typePack, sizePack, valuePack)
+        rows.append(struct.pack("!ii%s" %
+                    (typeSymbol), typePack, sizePack, valuePack))
+    # pk is the row to update, version is
+    # implement me, pack is done here
+    # returns new version code
+    print("successfully packed")
+    result = b''.join(
+        [request, key, count, b''.join(rows)])
+    print(result)
+    return result
+
 
 # command -> function
 # functions must have the following signature: funcName(tableNumber, keyArg=val)->byteArray
 switcher = {
     INSERT: insertReq,
     DROP: dropReq,
-    EXIT: exitReq
+    EXIT: exitReq,
+    UPDATE: updateReq
 }
-    
+
+
 def request(sock, command=1, table_nr=0, **kwargs):
     req = switcher[command]
     # IMPORTANT: req functions take a tableNumber and any number of keyward arguments
     return sock.send(req(table_nr, **kwargs))
-    
+
+
 def response(sock, command=0):
-    # get response code 
+    # get response code
     responseCode = struct.unpack("!i", sock.recv(4))[0]
+    print("responseCode = ", responseCode)
     if responseCode != OK:
         raise responseCodeErrors[responseCode]()
     # get and unpack the expected byte sequence based on the command's response structure
@@ -119,8 +172,18 @@ def response(sock, command=0):
         bufPack = 'QQ'
         bufSize = 16
         buffer = sock.recv(bufSize)
-        pk, version = struct.unpack("!%s"%(bufPack), buffer)
+        pk, version = struct.unpack("!%s" % (bufPack), buffer)
         return pk, version
     if command in (DROP, EXIT):
         return
+    if command == UPDATE:
+        print("reading")
+        bufPack = 'Q'
+        bufSize = 8
+        buffer = sock.recv(bufSize)
+        version = struct.unpack("!%s" % (bufPack), buffer)
+        print("returning")
+        return version
+    if command == GET:
+        print("getting")
     return responseCode
